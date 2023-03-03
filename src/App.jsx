791f933098c3b0f7238059707dc2e649fb0ba90b
Ivan Henriquez <ivan.henriquez@omegaccs.com>
2023-03-03 14:32:31 -0300

    actualizando react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,13 @@ import reactLogo from "./assets/react.svg";
 import { Navbar } from "./components/Navbar";
 import "./App.css";
 
-import { BrowserRouter } from "react-router-dom";
-
 import AppRoutes from "./Routes";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <AppRoutes />
-      </BrowserRouter>
+      <Navbar />
+      <AppRoutes />
     </div>
   );
 }
